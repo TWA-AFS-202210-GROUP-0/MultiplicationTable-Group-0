526030dae5cedcb0f103e8926ecccdc0c204fa8b
binrui <bmu2@slb.com>
2022-10-25 17:30:13 +0800

    test: add unhappy pass test for equation

--- src/MultiplicationTable.ts
+++ src/MultiplicationTable.ts
@@ -1,10 +1,12 @@
 export class MultiplicationTable {
   
-
-
-
   public render(start: number, end: number): string{
-    
+    if(this.IsValidStartandEndNumber(start, end)){
+      if(this.IsInRange(start, end)){
+        return this.equation(start, end);
+      }
+    }
+    return '';
   }
   private IsValidStartandEndNumber(start: number, end: number): boolean {
     return start <= end;
@@ -12,6 +14,14 @@ export class MultiplicationTable {
   private IsInRange(start: number, end: number): boolean {
     return (start >= 1) && (start <= 10) && (end >= 1) && (end <= 10);
   }
+  private getRange(start: number, end: number): number[]{
+    return Array.from({length: end-start+1}, (value, index) => start+ index);
+  }
+  private equation(num1: number, num2: number): string {
+    const result = num1 * num2;
+    return num1 + '*' + num2 + '=' + result;
+  }
+
 
 
 
@@ -19,12 +29,9 @@ export class MultiplicationTable {
 }
 
  
-export function MultipliTable(start: number, end: number): boolean {
+// export function MultipliTable(start: number, end: number): boolean {
 
-}
-export function Lines(start: number, lineNum: number): string {
+// }
+// export function Lines(start: number, lineNum: number): string {
 
-}
-export function Equation(start: number, end: number): string {
-  return `${start} * ${end} = ${start* end}`;
-}
+// }
--- test/MultiplicationTable.test.ts
+++ test/MultiplicationTable.test.ts
@@ -13,4 +13,16 @@ describe('Multiplication Table', () => {
     //then
     expect(rendered).toBe('1*1=1')
   })
+  it('should render multiplication table of (2, 1)', () => {
+    // given
+    const table = new MultiplicationTable()
+    const start = 2
+    const end = 1
+
+    //when
+    const rendered = table.render(start, end)
+
+    //then
+    expect(rendered).toBe('')
+  })
 })
