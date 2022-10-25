3fd0583c7cacdedf07c8758ca68b42b31c19ac29
Siqi Song <ssong6@slb.com>
2022-10-25 20:11:10 +0800

    feat: is valid input

--- src/MultiplicationTable.ts
+++ src/MultiplicationTable.ts
@@ -1,5 +1,44 @@
 export class MultiplicationTable {
   public render(start: number, end: number): string{
-    return '1*1=1'
+    const isValid = isValidInput(start,end)
+    
+    if (!isValid) {
+      return ''
+    }
+    else
+    {
+      return multiplicationTable(start,end)
+    }
+    
+  }
+
+  
+}
+
+function  isValidInput(start:number, end:number):boolean {
+  return isStartLessOrEqualThanEnd(start,end) && isInclusive(start,end)
+}
+function isStartLessOrEqualThanEnd(start: number, end: number):boolean {
+  return start<=end
+}
+
+function isInclusive(start: number, end: number): boolean {
+  return start<=10 && start>=1 && end<=10 && end>=1 
+}
+
+function multiplicationTable(start:number, end:number):string {
+  var resultTable=''
+  for (let index = start; index < end+1; index++) {
+      resultTable += LineExpression(start,index) 
+      resultTable += '\n' 
+  }
+  return resultTable
+}
+ 
+function LineExpression(start: number, end: number) :string {
+  for (let index = star; index < array.length; index++) {
+    const element = array[index];
+    
   }
 }
+
--- test/MultiplicationTable.test.ts
+++ test/MultiplicationTable.test.ts
@@ -13,4 +13,43 @@ describe('Multiplication Table', () => {
     //then
     expect(rendered).toBe('1*1=1')
   })
+
+  it('should return null string of (3, 2)', () => {
+    // given
+    const table = new MultiplicationTable()
+    const start = 3
+    const end = 2
+
+    //when
+    const rendered = table.render(start, end)
+
+    //then
+    expect(rendered).toBe('')
+  })
+
+  it('should return null string of (3, 11)', () => {
+    // given
+    const table = new MultiplicationTable()
+    const start = 3
+    const end = 11
+
+    //when
+    const rendered = table.render(start, end)
+
+    //then
+    expect(rendered).toBe('')
+  })
+
+  it('should return multiplication table of (2, 4)', () => {
+    // given
+    const table = new MultiplicationTable()
+    const start = 2
+    const end = 4
+
+    //when
+    const rendered = table.render(start, end)
+
+    //then
+    expect(rendered).toBe('2*2=4\n 2*3=6  3*3=9\n 2*4=8  3*4=12  4*4=16')
+  })
 })
