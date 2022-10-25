c963c404368f647e7ef10e1aed26048a85d2da7a
binrui <bmu2@slb.com>
2022-10-25 17:28:34 +0800

    feat: add validation steps for equation

--- src/MultiplicationTable.ts
+++ src/MultiplicationTable.ts
@@ -1,5 +1,30 @@
 export class MultiplicationTable {
+  
+
+
+
   public render(start: number, end: number): string{
-    return '1*1=1'
+    
   }
+  private IsValidStartandEndNumber(start: number, end: number): boolean {
+    return start <= end;
+  }
+  private IsInRange(start: number, end: number): boolean {
+    return (start >= 1) && (start <= 10) && (end >= 1) && (end <= 10);
+  }
+
+
+
+
+}
+
+ 
+export function MultipliTable(start: number, end: number): boolean {
+
+}
+export function Lines(start: number, lineNum: number): string {
+
+}
+export function Equation(start: number, end: number): string {
+  return `${start} * ${end} = ${start* end}`;
 }
